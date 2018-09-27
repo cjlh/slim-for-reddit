6615dbddef5d245a11693c4565d73774f4b04033
Caleb Hamilton <cjlhamilton@gmail.com>
2018-09-27 23:59:28 +0100

    Added comments to clarify magic number

--- src/static/script/comments.js
+++ src/static/script/comments.js
@@ -1,14 +1,15 @@
 function collapseComment(event) {
 	var element = event.target;
+	// navigate element hierarchy to get comment container 
 	for (var i = 0; i < 6; i++) {
 		element = element.parentNode;
 	}
 	if (element.classList.contains("comment-hidden")) {
-		// Show
+		// if hidden, show comment
 		event.target.innerHTML = "&ndash;";
 		element.classList.remove("comment-hidden");
 	} else {
-		// Hide
+		// if not hidden, hide comment
 		event.target.innerHTML = "+";
 		element.classList.add("comment-hidden");
 	}
